6e3a6a8de6f36c71f22de849ea5e58ed3b29a9dd
Joakim Carlstein <joakim@aboviq.com>
2021-01-15 09:22:32 +0100

    👷 This should do the trick

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,6 @@ git config user.name "$GIT_CONFIG_NAME"
 git config user.email "$GIT_CONFIG_EMAIL"
 git remote rm origin
 git remote add origin "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git add --all .
+git commit -m "🚀 Deploy"
 npm run deploy
